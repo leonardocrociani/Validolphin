8a1d75d66bc0893ca34dce66c2392adea3c02772
Leonardo Crociani <88234007+leonardocrociani@users.noreply.github.com>
2023-06-26 20:01:01 +0200

    Update Validolphin.js

--- Validolphin.js
+++ Validolphin.js
@@ -7,12 +7,17 @@ class Validolphin {
         Password: { name: 'password' },
         Domain: { name: 'domain' },
         Phone: { name: 'phone' },
+        Iban: { name: 'iban' },
+        CreditCard: { name: 'credit-card' }
     }
     static utils = {
         email_regular_expression: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
         password_regular_expression: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
         domain_regular_expression: /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/,
         phone_regular_expression: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+        iban_regular_expression: /^([A-Z]{2}[ \-]?[0-9]{2})(?=(?:[ \-]?[A-Z0-9]){9,30}$)((?:[ \-]?[A-Z0-9]{3,5}){2,7})([ \-]?[A-Z0-9]{1,3})?$/,
+        credit_card_regular_expression: /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/,
+        // matches : Visa, MasterCard, American Express, Diners Club, Discover, and JCB
     }
 
     constructor(configObject) {
@@ -170,6 +175,16 @@ class Validolphin {
                                         errors.push('The phone is not valid -> ' + value);
                                     }
                                 }
+                                if (configObj.schema[key].type == Validolphin.types.Iban) {
+                                    if (!Validolphin.utils.iban_regular_expression.test(value)) {
+                                        errors.push('The iban is not valid -> ' + value);
+                                    }
+                                }
+                                if (configObj.schema[key].type == Validolphin.types.CreditCard) {
+                                    if (!Validolphin.utils.credit_card_regular_expression.test(value)) {
+                                        errors.push('The credit card is not valid -> '+value);
+                                    }
+                                }
                             }
 
                         }
@@ -192,7 +207,7 @@ class Validolphin {
             | | / / /| | / /    / // / / / / / / /   / /_/ / /_/ // //  |/ / 
             | |/ / ___ |/ /____/ // /_/ / /_/ / /___/ ____/ __  // // /|  /  
             |___/_/  |_/_____/___/_____/\\____/_____/_/   /_/ /_/___/_/ |_/   
-            Version: 1.1.1
+            Version: 1.1.2
         `)
     }
 }
