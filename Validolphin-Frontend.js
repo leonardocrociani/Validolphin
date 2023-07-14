1c8e310348b7938d24d05687b70361c9cae2d7c9
Leonardo Crociani <88234007+leonardocrociani@users.noreply.github.com>
2023-07-14 10:23:36 +0200

    Update Validolphin-Frontend.js

--- Validolphin-Frontend.js
+++ Validolphin-Frontend.js
@@ -1,4 +1,3 @@
-class Validolphin {
 
     availableKeys = ['validables', 'unexpectedKeyError', 'errorsAtTime', 'missingKeyError'];
     availableValidablesKeys = ['name', 'schema'];
@@ -21,6 +20,7 @@ class Validolphin {
     }
 
     constructor(configObject) {
+
         // controllo configObj
         for (const key in configObject) {
             if (!this.availableKeys.includes(key)) {
@@ -37,11 +37,15 @@ class Validolphin {
             }
         }
 
+        if (configObject.unexpectedKeyError) { this.unexpectedKeyError = configObject.unexpectedKeyError };
+        if (configObject.errorsAtTime) { this.errorsAtTime = configObject.errorsAtTime };
+        if (configObject.missingKeyError) { this.missingKeyError = configObject.missingKeyError };
+
         for (const configObj of configObject.validables) {
             // registrazione funzioni e loro contenuto
             this[configObj.name] = toValid => {
                 try {
-                    const errors = [];
+                    const all_errors = [];
                     const required = [];
                     for (const key in configObj.schema) {
                         if (configObj.schema[key].nullable != true) {
@@ -49,26 +53,35 @@ class Validolphin {
                         }
                     }
                     for (const req of required) {
-                        if (!Object.keys(toValid).includes(req)) errors.push(`${configObj.missingKeyError ? configObj.missingKeyError : 'Missing required key'} -> ${req}`)
+                        if (!Object.keys(toValid).includes(req)) all_errors.push(`${this.missingKeyError ? this.missingKeyError : 'Missing required key'} -> ${req}`)
                     }
                     for (const key in toValid) {
                         if (!Object.keys(configObj.schema).includes(key)) {
-                            return { valid: false, error: `${configObj.unexpectedKeyError ? configObj.unexpectedKeyError : 'Unexpected key'} -> ${key}` }
+                            return { valid: false, error: `${this.unexpectedKeyError ? this.unexpectedKeyError : 'Unexpected key'} -> ${key}` }
                         }
                         else {
                             const value = toValid[key];
+                            const err = configObj.schema[key]['err'];
+                            const errors = [];
                             if ((value == null || value == undefined) && configObj.schema[key].nullable != true) {
-                                errors.push('Not nullable value, find null -> ', value);
+                                if (err) return { valid: false, error: err }
+                                else all_errors.push('Not nullable value, find null -> ', value);
                                 continue
                             }
-                            if (value) {
+                            if (value!=null&&value!=undefined) {
                                 // general type check
                                 if (configObj.schema[key].type) {
                                     if (
-                                        typeof value != configObj.schema[key].type.name.toLowerCase() &&
-                                        ('array' == configObj.schema[key].type.name.toLowerCase() && !Array.isArray(value))
+                                        typeof value != configObj.schema[key].type.name.toLowerCase()
                                     ) {
-                                        errors.push(`${configObj.typeError ? configObj.typeError : 'Type error: expected ' + configObj.schema[key].type.name + ', received ' + typeof value + ' with the value'} -> ${value}`);
+                                        if ('array' == configObj.schema[key].type.name.toLowerCase()) {
+                                            if (!Array.isArray(value)) {
+                                                errors.push(`${configObj.typeError ? configObj.typeError : 'Type error: expected ' + configObj.schema[key].type.name + ', received ' + typeof value + ' with the value'} -> ${value}`);
+                                            }
+                                        }
+                                        else {
+                                            errors.push(`${configObj.typeError ? configObj.typeError : 'Type error: expected ' + configObj.schema[key].type.name + ', received ' + typeof value + ' with the value'} -> ${value}`);
+                                        }
                                     }
                                 }
                                 // specific type check
@@ -94,6 +107,12 @@ class Validolphin {
                                         if (value > configObj.schema[key].maxVal) {
                                             errors.push('Minimum value not respected for key -> ' + key);
                                         }
+                                    }
+                                    if (isNaN(value)) {
+                                        errors.push('Illegal NaN value found -> '+key);
+                                    }
+                                    if (isNaN(value+1)) {
+                                        errors.push('Very illegal NaN value fround ->'+key);
                                     }
                                 }
                                 if (configObj.schema[key].type == Array) {
@@ -182,15 +201,23 @@ class Validolphin {
                                 }
                                 if (configObj.schema[key].type == Validolphin.types.CreditCard) {
                                     if (!Validolphin.utils.credit_card_regular_expression.test(value)) {
-                                        errors.push('The credit card is not valid -> '+value);
+                                        errors.push('The credit card is not valid -> ' + value);
                                     }
                                 }
                             }
-
+                            else {
+                                if (configObj.schema[key].nullable != true) {
+                                    errors.push('Illegal value -> '+value+', key, '+key);
+                                }
+                            }
+                            if (errors.length > 0) {
+                                if (err) return { valid: false, error: err }
+                                else all_errors.push(...errors);
+                            }
                         }
                     }
-                    if (errors.length > 0) {
-                        return { valid: false, error: errors.splice(0, (configObj.errorsAtTime ? configObj.errorsAtTime : 3)).join(' |===| ').trimEnd() }
+                    if (all_errors.length > 0) {
+                        return { valid: false, error: all_errors.splice(0, (this.errorsAtTime ? this.errorsAtTime : 3)).join(' |===| ').trimEnd() }
                     }
                     return { valid: true }
                 } catch (err) {
@@ -207,7 +234,7 @@ class Validolphin {
             | | / / /| | / /    / // / / / / / / /   / /_/ / /_/ // //  |/ / 
             | |/ / ___ |/ /____/ // /_/ / /_/ / /___/ ____/ __  // // /|  /  
             |___/_/  |_/_____/___/_____/\\____/_____/_/   /_/ /_/___/_/ |_/   
-            Version: 1.1.2
+            Version: 1.2.0
         `)
     }
 }
